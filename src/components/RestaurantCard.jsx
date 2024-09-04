8260f373d256e2929bf7f3a179e11a30f993333c
Veeresh Biradar <veeresh.r.biradar@outlook.com>
2024-09-04 22:27:04 +0530

    Restaurant Card Design Updated & Data Added

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,20 +1,27 @@
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+
+  const { resData } = props; 
+
+  const { name, place, breakfast, dinner, rating, imageId} = resData;
+
   return (
     <div className="restaurant-card">
       <div className="restaurant-card__image-container">
         <img
-          src="https://via.placeholder.com/150"
+          src={"https://media-assets.swiggy.com/swiggy/image/upload/"+imageId}
           alt="restaurant-card"
           className="restaurant-card__image"
         />
       </div>
       <div className="restaurant-card__info">
-        <h3 className="restaurant-card__name">Namste Restaurant</h3>
-        <p className="restaurant-card__cuisine">Cusine</p>
+        <h2 className="restaurant-card__name">{name}</h2>
+        <h3 className="restaurant-card__cuisine">{place}</h3>
         <div className="restaurant-card__details">
-          <span className="restaurant-card__rating">3.3 ★</span>
+          <span className="restaurant-card__rating">{rating} ★</span>
           <span className="restaurant-card__delivery-time">5.89 mins</span>
         </div>
+        <h4 className="restaurant-card__cuisine">{dinner}</h4>
+        <h5 className="restaurant-card__cuisine">{breakfast}</h5>
       </div>
     </div>
   );
